f4e93eb17127dbe21283281a2fd3c7346af1cc43
Maurocazzato <maurizzio.125@gmail.com>
2023-11-23 11:53:00 +0100

    confronto e conferma

--- js/script.js
+++ js/script.js
@@ -4,4 +4,32 @@
 const bottonvai = document.getElementById("clickmail");
 
 // email
-const emailArray = ("mauro@example.it")
+const emailArray = ("mauro@example.it","andrea@example.it","mario@example.it");
+
+bottonvai.addEventListener("click", function(){
+
+let trovato = false;
+
+const emailUser = document.getElementById("email").value;
+
+// controllo se email presente
+
+for( let i = 0; i < emailArray.length; i++){
+
+    // I-ESIMO ELEMENTO DI array
+    const email = emailArray[i];
+    console.log(email);
+
+    // confronto con elemento inserito
+
+    if (email.toLowerCase() === emailUser.toLowerCase()){
+        trovato = true;
+    }
+}
+
+    if(trovato == true) {
+        confirm("accesso eseguito");
+    } else{
+        confirm("accesso negato. ritenta");
+    }
+})
