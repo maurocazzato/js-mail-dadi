506df72067b35bd94f0b8789e718ef0b7247b40c
Maurocazzato <maurizzio.125@gmail.com>
2023-11-23 10:15:51 +0100

    creazione impostazione pagine

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+
+// creo il bottone
+const bottonvai = document.getElementById("clickmail");
+
+// email
+const emailArray = ("mauro@example.it")
